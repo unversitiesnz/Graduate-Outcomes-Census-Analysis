ffbb69f49eb2bb29366aac2deb6cea99e4477647
Scott Henwood <scott.henwood@universitiesnz.ac.nz>
2020-03-04 16:37:53 +1300

    Work on GO website db population

--- scripts/Delete occupation related tables.sql
+++ scripts/Delete occupation related tables.sql
@@ -1,11 +1,25 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
-delete
-  FROM [GradOutcomesNZv5-0].[dbo].[CareersNZInfo]
-delete
-  FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5VocationalPathway]
-  delete
-  FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5NarrowFieldOfStudyAlternative]
-delete
-  FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5NarrowFieldOfStudy]
-delete
-  FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5]
+delete FROM [GradOutcomesNZv5-0].[dbo].[CareersNZInfo]
+delete FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5VocationalPathway]
+delete FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5NarrowFieldOfStudyAlternative]
+delete FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5NarrowFieldOfStudy]
+delete FROM [GradOutcomesNZv5-0].[dbo].[ANZSCOOccupationL5]
+
+delete from ANZSCOOccupationL4
+delete from ANZSCOOccupationL3
+delete from ANZSCOOccupationL2
+delete from ANZSCOOccupationL1
+
+delete from VocationalPathway
+
+delete from AnnualIncome
+delete from NarrowFieldOfStudyLevel
+
+delete from NarrowFieldOfStudy
+
+delete from BroadFieldOfStudy
+
+
+
+
+
